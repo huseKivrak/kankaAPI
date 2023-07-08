174966fee4db7400c93721d329f25301ea145884
Huse Kivrak <huse7@pm.me>
2023-07-08 00:46:52 -0400

    APScheduler added; first test letter worked

--- project/core/apps.py
+++ project/core/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        print("Starting delivery scheduler")
+        from delivery_scheduler import letter_updater
+        letter_updater.start()
+
