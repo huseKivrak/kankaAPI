d01faf468cf3428681f27847adda120c7372dad9
Huse Kivrak <huse7@pm.me>
2023-07-08 22:02:46 -0400

    reformat

--- project/delivery_scheduler/letter_updater.py
+++ project/delivery_scheduler/letter_updater.py
@@ -2,15 +2,21 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from core.models import Letter
 from django.utils import timezone
 
+
 def deliver_letters():
     letters = Letter.letters.sents()
     for letter in letters:
         if letter.delivery_date < timezone.now():
             letter.deliver()
 
+
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(deliver_letters, 'interval', minutes=1, id='deliver_letters', replace_existing=True)
+    scheduler.add_job(
+        deliver_letters,
+        'interval',
+        minutes=1,
+        id='deliver_letters',
+        replace_existing=True)
+
     scheduler.start()
-
-
